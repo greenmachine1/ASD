94823009cfc1c9516c490869c494131fa8e0ef9c
Cory Green <greenmachine@fullsail.edu>
2012-06-02 02:03:13 -0700

    hourly commit, modifying my code to best suit jquery.  Its starting to finally click now

--- Gold/js/main.js
+++ Gold/js/main.js
@@ -127,14 +127,14 @@ function toggleControls(n)
 	switch(n)
 	{
 		case "on":
-			$('bandInfo').style.display = "none";
-			$('reset').style.display = "inline";
-			$('disp').style.display = "none";
-			$('addNew').style.display = "inline";
+			$('bandInfo').css('display', "none");  //style.display = "none";
+			$('reset').css('display', "inline");
+			$('disp').css('display', "none");
+			$('addNew').css('display', "inline");
 			break;
 		case "off":
-			$('bandInfo').style.display = "block";
-			$('reset').style.display = "inline";
+			$('bandInfo').css('display', "block");
+			$('reset').css('display', "inline");
 			$('disp').style.display = "inline";
 			$('addNew').style.display = "inline";
 			$('item').style.display = "inline";
@@ -196,33 +196,33 @@ function toggleControls(n)
 			alert("There is no data in local storage so default data was added.");
 			autoFillData();
 		}
-		var makeDiv = document.createElement('div');
-		makeDiv.setAttribute("id", "item");
-		var makeList = document.createElement('ul');
-		makeDiv.appendChild(makeList);
-		document.body.appendChild(makeDiv);
-		$('item').style.display = "block";
+		var makeDiv = $('<div>');  //document.createElement('div');
+		makeDiv.attr("id", "item");  // setAttribute("id", "item");
+		var makeList = $('<ul>');	//createElement('ul');
+		makeDiv.append(makeList);
+		$('#ending').appendChild(makeDiv);					//document.body.appendChild(makeDiv);	// this might be 																//creating a problem as it is trying to append a div to the 															//body...yeah...
+		$('#item').style.display = "block";
 		for(var i = 0, j = localStorage.length; i < j; i++)
 		{
-			var makeli = document.createElement('li');
-			var linksLi = document.createElement('li');
-			makeList.appendChild(makeli);
+			var makeli = $('<li>');
+			var linksLi = $('<li>');
+			makeList.append(makeli);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			
 			// convert the string fromm local storage value back to an object by using JSON .parse
 			var obj = JSON.parse(value);
-			var makeSubList = document.createElement('ul');
-			makeli.appendChild(makeSubList);
+			var makeSubList = $('<ul>');  //document.createElement('ul');
+			makeli.append(makeSubList);
 			
 			getImage(obj.groups[1], makeSubList); // gets an image for our form, passes in makeSubList
 			for(var n in obj)	// changed item from obj
 			{
-				var makeSubli = document.createElement('li');
-				makeSubList.appendChild(makeSubli);
+				var makeSubli = $('<li>');  //document.createElement('li');
+				makeSubList.append(makeSubli);
 				var optSubText = obj[n][0]+" "+obj[n][1];
-				$('makeSubli').html(optSubText); 							// makeSubli.innerHTML = optSubText;
-				makeSubList.appendChild(linksLi);
+				makeSubli.html(optSubText); 							// makeSubli.innerHTML = optSubText;
+				makeSubList.append(linksLi);
 			}
 			makeItemLinks(localStorage.key(i), linksLi); // create our edit and delete buttons/links for each item in local storage
 		}	
@@ -231,11 +231,13 @@ function toggleControls(n)
 	// get the image for the right catagory
 	function getImage(catName, makeSubList)
 	{
-		var imageLi = document.createElement('li');
-		makeSubList.appendChild(imageLi);
-		var newImg = document.createElement('img');
-		var setSrc = newImg.setAttribute("src", "pics/"+ catName + ".png");
-		imageLi.appendChild(newImg);
+		//creates a ul then appends an li to it then appends an image to that
+		
+		var imageLi = $('<li>');  //document.createElement('li');
+		$('<ul>').append(imageLi); // makeSubList.appendChild(imageLi);
+		var newImg = $('<img>');  //document.createElement('img');
+		var setSrc = newImg.attr("src", "pics/"+ catName + ".png");
+		imageLi.append(newImg);
 	}
 	
 	// autopopulate the local storage
@@ -255,25 +257,25 @@ function toggleControls(n)
 	function makeItemLinks(key, linksLi)
 	// add edit single item link
 	{
-		var editLink = document.createElement('a');
+		var editLink = $('<a>'); //document.createElement('a');
 		editLink.href = "#";
 		editLink.key = key;	
 		var editText = "Edit Info";
-		editLink.addEventListener("click", editItem);
+		editLink.bind("click", editItem); //addEventListener("click", editItem);
 		$('editLink').html(editText);    // editLink.innerHTML = editText;
-		linksLi.appendChild(editLink);
+		linksLi.append(editLink);
 		
 		// adds a seperator between links
-		var breakTag = document.createElement('br');
-		linksLi.appendChild(breakTag);
+		var breakTag = $('<br>');  //document.createElement('br');
+		linksLi.append(breakTag);
 		
-		var deleteLink = document.createElement('a');
+		var deleteLink = $('<a>'); //document.createElement('a');
 		deleteLink.href = "#";
 		deleteLink.key = key;
 		var deleteText = "Delete Info";
-		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.bind("click", deleteItem);
 		$('deleteLink').html(deleteText);			// deleteLink.innerHTML = deleteText;
-		linksLi.appendChild(deleteLink);
+		linksLi.append(deleteLink);
 	}
 	
 	function editItem()
@@ -286,11 +288,11 @@ function toggleControls(n)
 		toggleControls("off");
 		
 		// populate the form fields with the current localStorage values.
-		$('fname').value = item.fname[1];
-		$('bname').value = item.bname[1];
-		$('email').value = item.email[1];
-		$('groups').value = item.groups[1];
-		$('startdate').value = item.startdate[1];
+		$('fname').val(item.fname[1]); // .value = item.fname[1];
+		$('bname').val(item.bname[1]);
+		$('email').val(item.email[1]);
+		$('groups').val(item.groups[1]);
+		$('startdate').val(item.startdate[1]);
 	if($('#inst1').checked)
 	{
 		instrument1Value = $('#inst1').value;
@@ -364,17 +366,17 @@ function toggleControls(n)
 		instrument9Value = "No";
 	} 
 		
-		$('other1').value = item.other1[1];
-		$('tickets').value = item.tickets[1];
+		$('other1').val(item.other1[1]);
+		$('tickets').val(item.tickets[1]);
 		
 		// remove the initial listener from the input save conctact
-		submit.removeEventListener("click", storeData);  // changed save to submit
+		submit.unbind("click", storeData);  // changed save to submit removeEventListener("click", 																 //storeData);
 		// change submit button value to edit button
-		$('submit').value = "Edit Contact";
+		$('submit').val("Edit Contact");
 		var editSubmit = $('submit');
 		// save key value established in this function as a property of the edit submit event
 		// so we can use that value when we save the data we edited.
-		editSubmit.addEventListener("click", validate);
+		editSubmit.bind("click", validate); //addEventListener("click", validate);
 		editSubmit.key = this.key;
 	}
 	
@@ -490,15 +492,15 @@ function toggleControls(n)
 	// set link and submit click events
 	// made all my links jquery links insead of normal javascript
 	// shows data function
-	$('disp').bind('click', getData);
+	$('#disp').bind('click', getData);
 	
 	
 	// set submit click events
-	$('submit').bind('click', validate);
+	$('#submit').bind('click', validate);
 	
 	
 	// clear function
-	$('reset').bind('click', clearLocal);
+	$('#reset').bind('click', clearLocal);
 	});
 
 
