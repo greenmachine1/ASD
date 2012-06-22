1ee190bcc16d245ad7e96389629db29bd124610b
Cory Green <greenmachine@fullsail.edu>
2012-06-21 17:14:24 -0700

    Made my couch adjustments to my main files

--- Gold/main.js
+++ Gold/main.js
@@ -3,9 +3,6 @@
 // Date: 05/30/2012
 // project: ASD
 
-//	waiting until the DOM is ready
-//  this beginning function has been modified to fit the jquery version
-
 // function used to split the url to retrieve the id name :D
 var urlVars = function(urlData){
 	var urlData = $($.mobile.activePage).data("url");
@@ -29,7 +26,7 @@ var urlVars = function(urlData){
 // When passing the data from the #news page to this newBandPage, upon loading, do this!
 $('#newBandPage').live("pageshow", function(){
 	var newBandPage = urlVars()["newBandPage"];
-	//console.log(newBandPage);
+	console.log(newBandPage);
 	$.couch.db("gigbag").view("plugin/Bands", {
 		key: newBandPage,
 	})
@@ -37,27 +34,157 @@ $('#newBandPage').live("pageshow", function(){
 	// works to retreive the object!
 	$.couch.db("gigbag").openDoc("Band:"+ newBandPage, {
 		success: function(data) {
-			console.log(data);
+
 			var bandName = data.bname[1];
 			var nameOfPerson = data.fname[1];
 			var genre = data.groups[1];
 			var email = data.email[1];
-			console.log(bandName + " " + nameOfPerson + " " + genre + " " + email);
 			
-	},
+			
+			$('#nameFirst1').val(nameOfPerson);
+			$('#bandNameFirst1').val(bandName);
+			$('#genreFirst1').val(genre);
+			$('#emailFirst1').val(email);
+			
+		},
 		error: function(status){
 			console.log(status);
 	}
-	})
+	
+	});	
+	
+// my delete function	
+ var deleteEntry = function(){
+	 
+	 var nameOfPerson1 = $('#nameFirst').val();
+	 var nameOfBand = $('#bandNameFirst').val();
+	 var nameOfGenre = $('#genreFirst').val();
+	 var nameOfEmail = $('#emailFirst').val(); 
+	 
+	// used to get the rev id 
+	$.couch.db("gigbag").openDoc("Band:"+ newBandPage,{
+		success: function(data) {
+			var rev = data._rev;
+	//	}
+	// }) 
+	 
+	 var doc = {
+		_id: "Band:"+nameOfBand,
+		_rev: rev,
+		bname: ["Band Name:", nameOfBand],
+		email: ["Email:", nameOfEmail],
+		fname: ["First name:", nameOfPerson1],
+		groups: ["Genre", nameOfGenre]
+	 };
+	 		// request to delete data
+	 		$.couch.db("gigbag").removeDoc(doc, {
+	 			success: function(data){
+	 				alert("This band Has been deleted!");
+	 			}
+	 		});
+		}
+	});
+	
+ }
+ 	$('#deleteInfoButton').bind('click', deleteEntry);
+ 
+	// the saveInfo button click event
+	$('#saveInfoButton1').bind('click', saveInfoEdit);
+	
+	$('#newBandPageItems').listview('refresh');
 });
+
+//setting up to fill in my input fields with the JSON data
+var saveInfo = function(){
+
+var nameOfPerson1 = $('#nameFirst').val();
+var nameOfBand = $('#bandNameFirst').val();
+var nameOfGenre = $('#genreFirst').val();
+var nameOfEmail = $('#emailFirst').val(); 
+		
+	// creation of a new document object to hold all my variable json data
+	var doc = {
+		_id: "Band:"+nameOfBand,
+		bname: ["Band Name:", nameOfBand],
+		email: ["Email:", nameOfEmail],
+		fname: ["first name:", nameOfPerson1],
+		groups: ["Genre:", nameOfGenre]
+	 }; // object closer
+				
+	// calling on the saveDoc function
+	// then alerting the user on success or failure
+	$.couch.db("gigbag").saveDoc(doc, {
+		success: function(data) {
+			alert("Document saved");
+			
+				        
+		}, // function success closer
+		error: function(status) {
+			console.log(status);
+			
+		} // error closer
+		
+	}); // total couch.db closer
+	
+	
+} // total save info function closer 
+
+// save info for my edit page
+var saveInfoEdit = function(){
+	
+	// get values from updated edits page!
+	var nameOfPerson1 = $('#nameFirst1').val();
+	var nameOfBand = $('#bandNameFirst1').val();
+	var nameOfGenre = $('#genreFirst1').val();
+	var nameOfEmail = $('#emailFirst1').val(); 
+			
+	
+	// goes through the data base so I can get the rev number
+	$.couch.db("gigbag").openDoc("Band:"+ nameOfBand,{
+		success: function(data) { 
+			
+		// need to get the band name back, if the person enters in a new band name, it messes up
+			
+		// creation of a new document object to hold all my variable json data
+		var doc = {
+			_id: "Band:"+nameOfBand,
+			_rev: data._rev,
+			bname: ["Band Name:", nameOfBand],
+			email: ["Email:", nameOfEmail],
+			fname: ["first name:", nameOfPerson1],
+			groups: ["Genre:", nameOfGenre]
+		 }; // object closer
+					
+		// calling on the saveDoc function
+		// then alerting the user on success or failure
+		$.couch.db("gigbag").saveDoc(doc, {
+			success: function(data) {
+				alert("Document saved");
+				
+					        
+			}, // function success closer
+			error: function(status) {
+				console.log(status);
+				
+			} // error closer
+			
+		}); // total couch.db closer
+		}
+	});
+		
+		
+	} // total save info function closer 
+
+
 
 
 // this function upon loading of the #news page retrieves the id of all the objects
 $('#news').live("pageshow", function(){
 	$.couch.db("gigbag").view("plugin/Bands", {
 		success:function(data){
+		
 			$('#localBandList').empty();
-			$.each(data.rows, function(indexm,value){
+			$.each(data.rows, function(index,value){
 				var item = (value.value || value.doc);
 				$('#localBandList').append(
 						$('<li>').append(
@@ -68,6 +195,8 @@ $('#news').live("pageshow", function(){
 				);
 			});
 			$('#localBandList').listview('refresh');
+			
+			$('#saveInfoButton').bind('click', saveInfo);
 		}
 		
 	});
@@ -643,4 +772,8 @@ function toggleControls(n)
 	$('#reset').bind('click', clearLocal);
 	*/
 //});
+
+
+//mucettereedgethentallyet
+// https://greenmachine:DccxQMgeQjneYsMiQBWrKvPs@greenmachine.cloudant.com/gigbag/
 
